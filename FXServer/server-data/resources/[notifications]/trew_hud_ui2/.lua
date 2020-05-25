982d4393b828e00cc7cf5c9f03c7bd8dafdba690
Benik1234 <65073847+Benik1234@users.noreply.github.com>
2020-05-25 12:25:21 -0700

    CC2

---
+++ FXServer/server-data/resources/[notifications]/trew_hud_ui2/.lua
@@ -0,0 +1,56 @@
+Config = {}
+
+Config.Locale = 'en'
+
+Config.serverLogo = 'https://i.imgur.com/AcgDL9f.png'
+
+Config.font = {
+	name 	= 'Montserrat',
+	url 	= 'https://fonts.googleapis.com/css?family=Montserrat:300,400,700,900&display=swap'
+}
+
+Config.date = {
+	format	 	= 'default',
+	AmPm		= false
+}
+
+Config.voice = {
+
+	levels = {
+		default = 5.0,
+		shout = 12.0,
+		whisper = 1.0,
+		current = 0
+	},
+	
+	keys = {
+		distance 	= '~',
+	}
+
+
+}
+
+Config.ui = {
+	showServerLogo		= false,
+
+	showJob		 		= true,
+
+	showWalletMoney 	= false,
+	showBankMoney 		= false,
+	showBlackMoney 		= false,
+	showSocietyMoney	= true,
+
+	showDate 			= false,
+	showLocation 		        = false,
+	showVoice	 		= false,
+
+	showHealth			= false,
+	showArmor	 		= false,
+	showStamina	 		= false,
+	showHunger 			= false,
+	showThirst	 		= false,
+
+	showMinimap			= false,
+
+	showWeapons			= true,	
+}
---
+++ FXServer/server-data/resources/[ribs]/new_banking/locales/en.lua
@@ -0,0 +1,13 @@
+Locales['en'] = {
+    ['invalid_amount'] = 'Nespravna castka',
+    ['atm_blip'] = 'ATM',
+    ['bank_blip'] = 'Bank',
+    ['atm_open'] = 'Zmackni ~INPUT_PICKUP~ Pro vlozeni karty ~b~',
+    ['no_money'] = 'Nemas dostatek penez',
+    ['recieved1'] = 'Prisli ti penize ',
+    ['recieved2'] = 'Prisla ti suma ',
+    ['recieved3'] = ' dollar',
+    ['removed1'] = 'Castka zamitnuta ',
+    ['removed2'] = 'Your payment of ',
+    ['removed3'] = ' dollars is done'
+}
